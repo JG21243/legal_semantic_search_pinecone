549e025a549c41c4a49e4ede9ba7d28d6e56730b
Josh Glen <129418264+JG21243@users.noreply.github.com>
2024-11-20 02:17:38 +0000

    Add Geist font support and update dependencies for improved styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { GeistSans } from 'geist/font/sans';
+import { GeistMono } from 'geist/font/mono';
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Pinecone Legal semantic search sample app",
@@ -15,8 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${GeistSans.variable} font-sans antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
